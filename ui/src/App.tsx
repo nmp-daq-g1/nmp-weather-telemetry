29f0cdb3d8f9c1bd897754c1b4f73a066c07878b
Charlene Kwok <charlenekwok499@gmail.com>
2022-03-14 00:34:57 +1100

    finished header

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import classes from './App.module.css';
+import NavBar from './components/navBar/NavBar';
 
 const App: React.FC = () => {
-  return <div>hello</div>;
+  const [mode, setMode] = useState(localStorage.getItem('colorMode') || 'dark');
+
+  const switchMode = (checked: boolean): void => {
+    const newMode = checked ? 'dark' : 'light';
+    setMode((): string => newMode);
+    localStorage.setItem('colorMode', newMode);
+  };
+
+  useEffect(() => {
+    if (!localStorage.getItem('colorMode')) {
+      localStorage.setItem('colorMode', 'dark');
+    }
+  }, []);
+
+  return (
+    <div
+      color-mode={localStorage.getItem('colorMode') || 'dark'}
+      className={classes.container}
+    >
+      <NavBar switchMode={switchMode} currMode={mode} />
+    </div>
+  );
 };
 
 export default App;
--- ui/src/components/navBar/NavBar.tsx
+++ ui/src/components/navBar/NavBar.tsx
@@ -1,8 +1,38 @@
 import React from 'react';
 import classes from './NavBar.module.css';
+import Logo from '../../assets/redbackLogo.svg';
+import SearchIcon from '@mui/icons-material/Search';
+import { DarkModeSwitch } from 'react-toggle-dark-mode';
 
-const NavBar: React.FC = () => {
-  return <div className={classes.container}>NavBar</div>;
+// checked === true => light
+// checked === false => dark
+const NavBar: React.FC<{
+  switchMode: (checked: boolean) => void;
+  currMode: string;
+}> = ({ switchMode, currMode }) => {
+  return (
+    <header className={classes.container}>
+      <img src={Logo} alt="redback logo" />
+      <div className={classes.items}>
+        <label htmlFor="search">
+          <SearchIcon htmlColor={currMode === 'light' ? 'black' : 'white'} />
+          <input id="search" placeholder="Search" />
+        </label>
+        <DarkModeSwitch
+          style={{
+            backgroundColor: 'transparent',
+            height: '30px',
+            marginTop: '7px',
+          }}
+          checked={localStorage.getItem('colorMode') === 'dark' || false}
+          onChange={switchMode}
+          size={120}
+          moonColor="white"
+          sunColor="#FFBD35"
+        />
+      </div>
+    </header>
+  );
 };
 
 export default NavBar;
