da886f4878bea1c1b5550f57ceb609889bcca1d6
Charlene Kwok <charlenekwok499@gmail.com>
2022-03-16 12:58:49 +1100

    add button that can go back original postition of widgets

--- ui/src/components/dashboard/Dashboard.tsx
+++ ui/src/components/dashboard/Dashboard.tsx
@@ -1,18 +1,18 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classes from './Dashboard.module.css';
 import TempWidget from './widgets/TempWidget';
 import WindWidget from './widgets/WindWidget';
 import WeatherWidget from './widgets/WeatherWidget';
 import RainWidget from './widgets/RainWidget';
 import GraphWidget from './widgets/GraphWidget';
+import ReplayIcon from '@mui/icons-material/Replay';
 import { Responsive, WidthProvider } from 'react-grid-layout';
 import '/node_modules/react-grid-layout/css/styles.css';
 import '/node_modules/react-resizable/css/styles.css';
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
 const Dashboard: React.FC = () => {
-  const layout = {
+  const defaultLayout = {
     lg: [
       { i: 'temp', x: 0, y: 0, w: 1, h: 3 },
       { i: 'wind', x: 1, y: 0, w: 1, h: 2 },
@@ -21,6 +21,29 @@ const Dashboard: React.FC = () => {
       { i: 'graph', x: 2, y: 1, w: 2, h: 3 },
     ],
   };
+
+  const [gridLayout, setGridLayout] = useState(
+    JSON.parse(localStorage.getItem('layout') || '{}') || defaultLayout,
+  );
+
+  useEffect(() => {
+    if (!localStorage.getItem('layout')) {
+      localStorage.setItem('layout', JSON.stringify(defaultLayout));
+    }
+  }, []);
+
+  const backOrigin = (): void => {
+    setGridLayout(defaultLayout);
+    localStorage.setItem('layout', JSON.stringify(defaultLayout));
+  };
+
+  const changedLayoutHandler = (
+    currentLayout: ReactGridLayout.Layout[],
+  ): void => {
+    setGridLayout({ lg: currentLayout });
+    localStorage.setItem('layout', JSON.stringify({ lg: currentLayout }));
+  };
+
   return (
     <section className={classes.wrapper}>
       <div className={classes.top}>
@@ -31,10 +54,11 @@ const Dashboard: React.FC = () => {
         </div>
       </div>
       <ResponsiveReactGridLayout
-        layouts={layout}
+        layouts={gridLayout}
         breakpoints={{ lg: 1200, sm: 768, xs: 480 }}
         cols={{ lg: 3, sm: 2, xs: 1 }}
         margin={[20, 20]}
+        onLayoutChange={changedLayoutHandler}
       >
         <div key="temp" className={classes.temp}>
           <TempWidget />
@@ -52,6 +76,13 @@ const Dashboard: React.FC = () => {
           <GraphWidget />
         </div>
       </ResponsiveReactGridLayout>
+      <button
+        className={classes.back}
+        title="Back to original position of widgets"
+        onClick={backOrigin}
+      >
+        <ReplayIcon className={classes['back-icon']} />
+      </button>
     </section>
   );
 };
