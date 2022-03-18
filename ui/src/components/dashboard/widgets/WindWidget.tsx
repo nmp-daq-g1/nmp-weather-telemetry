134ab08efa38bea95fa5b0f591355f6c1394a7b7
Charlene Kwok <charlenekwok499@gmail.com>
2022-03-18 23:16:12 +1100

    changed to dot notation

--- ui/src/components/dashboard/widgets/WindWidget.tsx
+++ ui/src/components/dashboard/widgets/WindWidget.tsx
@@ -7,10 +7,8 @@ import Pointer from '../../../assets/Pointer';
 const WindWidget: React.FC = () => {
   const ctx = useContext(data);
   const directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
-  // eslint-disable-next-line @typescript-eslint/dot-notation
-  const dir = Math.round(ctx.realTimeData['wind_speed']);
-  // eslint-disable-next-line @typescript-eslint/dot-notation
-  let angle = ctx.realTimeData['wind_dir'];
+  const dir = Math.round(ctx.realTimeData.wind_speed);
+  let angle = ctx.realTimeData.wind_dir;
   const index = Math.round(((angle %= 360) < 0 ? angle + 360 : angle) / 45) % 8;
 
   return (
